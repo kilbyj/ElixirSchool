c400c6ed0b2382a9855ab9a1ec1ac8d03f6ba5b5
Jonathan Kilby <jonathan.kilby1991@gmail.com>
2022-07-29 14:21:52 -0400

    Chat

---
+++ chat/config/dev.exs
@@ -0,0 +1,2 @@
+import Config
+config :chat, remote_supervisor: fn(recipient) -> {Chat.TaskSupervisor, recipient} end
---
+++ chat/config/test.exs
@@ -0,0 +1,2 @@
+import Config
+config :chat, remote_supervisor: fn(_recipient) -> Chat.TaskSupervisor end
